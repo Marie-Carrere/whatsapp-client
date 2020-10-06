ef249b1f8649c22859fe071c160da72b012dd133
Marie-Carrere <marie.carrere@icloud.com>
2020-10-06 11:21:39 +0200

    :lipstick: Add style to Chats

--- src/components/ChatsListsScreen/ChatsList.tsx
+++ src/components/ChatsListsScreen/ChatsList.tsx
@@ -1,24 +1,82 @@
 import React from "react";
 import { chats } from "../../fixtures/db";
 import moment from "moment";
+import { List, ListItem } from "@material-ui/core";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: calc(100% - 56px);
+  overflow-y: overlay;
+`;
+
+const StyledList = styled(List)`
+  padding: 0 !important;
+`;
+
+const StyledListItem = styled(ListItem)`
+  height: 76px;
+  padding: 0 15px;
+  display: flex;
+`;
+
+const ChatPicture = styled.img`
+  height: 50px;
+  width: 50px;
+  object-fit: cover;
+  border-radius: 50%;
+`;
+
+const ChatInfo = styled.div`
+  width: calc(100% - 60px);
+  height: 46px;
+  padding: 15px 0;
+  margin-left: 10px;
+  border-bottom: 0.5px solid silver;
+  position: relative;
+`;
+
+const ChatName = styled.div`
+  margin-top: 5px;
+`;
+
+const MessageContent = styled.div`
+  color: gray;
+  font-size: 15px;
+  margin-top: 5px;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+`;
+
+const MessageDate = styled.div`
+  position: absolute;
+  color: gray;
+  top: 20px;
+  right: 0;
+  font-size: 13px;
+`;
 
 const ChatsList: React.FC = () => (
-  <div>
-    <ul>
+  <Container>
+    <StyledList>
       {chats.map((chat) => (
-        <li key={chat.id}>
-          <img src={chat.picture} alt={`${chat.name}'s profile`} />
-          <div>{chat.name}</div>
-          {chat.lastMessage && (
-            <>
-              <div>{chat.lastMessage.content}</div>
-              <div>{moment(chat.lastMessage.createdAt).format("HH:mm")}</div>
-            </>
-          )}
-        </li>
+        <StyledListItem key={chat.id} button>
+          <ChatPicture src={chat.picture} alt={`${chat.name}'s profile`} />
+          <ChatInfo>
+            <ChatName>{chat.name}</ChatName>
+            {chat.lastMessage && (
+              <>
+                <MessageContent>{chat.lastMessage.content}</MessageContent>
+                <MessageDate>
+                  {moment(chat.lastMessage.createdAt).format("HH:mm")}
+                </MessageDate>
+              </>
+            )}
+          </ChatInfo>
+        </StyledListItem>
       ))}
-    </ul>
-  </div>
+    </StyledList>
+  </Container>
 );
 
 export default ChatsList;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: "#2c6157" },
+    secondary: { main: "#6fd056" },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
