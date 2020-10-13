3bf2fe9703672b0d5d423c7278e2f18349249ed3
Marie-Carrere <marie.carrere@icloud.com>
2020-10-13 14:35:55 +0200

    :boom: Replace REST call with GraphQL call

--- src/components/ChatsListsScreen/ChatsList.tsx
+++ src/components/ChatsListsScreen/ChatsList.tsx
@@ -55,20 +55,43 @@ const MessageDate = styled.div`
   font-size: 13px;
 `;
 
+const getChatsQuery = `
+  query GetChats {
+    chats {
+      id
+      name
+      picture
+      lastMessage {
+        id
+        content
+        createdAt
+      }
+    }
+  }
+`;
+
 const ChatsList: React.FC = () => {
   const [chats, setChats] = useState<any[]>([]);
 
   // Run fetch only when the component has mounted
   useMemo(async () => {
-    const body = await fetch(`${process.env.REACT_APP_SERVER_URL}/chats`);
-    const chats = await body.json();
+    const body = await fetch(`${process.env.REACT_APP_SERVER_URL}/graphql`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ query: getChatsQuery })
+    });
+    const {
+      data: { chats }
+    } = await body.json();
     setChats(chats);
   }, []);
 
   return (
     <Container>
       <StyledList>
-        {chats.map((chat) => (
+        {chats.map(chat => (
           <StyledListItem key={chat!.id} button>
             <ChatPicture src={chat.picture} alt={`${chat.name}'s profile`} />
             <ChatInfo>
